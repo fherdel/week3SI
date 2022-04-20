0d6f62d726d26e6ea1866b045d58356aeb9e47a9
raul-rauqoz <raul.quinonez@telus.com>
2022-04-20 09:59:44 -0600

    feat: initial commit

--- dsu-si-week-3-front/src/store/user.reducer.js
+++ dsu-si-week-3-front/src/store/user.reducer.js
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 // Slice
 
@@ -8,20 +8,20 @@ import axios from "axios";
 //   : null
 
 const slice = createSlice({
-  name: "user",
-  initialState: {
-    user: null,
-  },
-  reducers: {
-    loginSuccess: (state, action) => {
-      state.user = action.payload;
-      //localStorage.setItem('user', JSON.stringify(action.payload))
-    },
-    logoutSuccess: (state, action) => {
-      state.user = null;
-      //localStorage.removeItem('user')
-    },
-  },
+	name: 'user',
+	initialState: {
+		user: null
+	},
+	reducers: {
+		loginSuccess: (state, action) => {
+			state.user = action.payload;
+			//localStorage.setItem('user', JSON.stringify(action.payload))
+		},
+		logoutSuccess: (state, action) => {
+			state.user = null;
+			//localStorage.removeItem('user')
+		}
+	}
 });
 
 export default slice.reducer;
@@ -33,30 +33,28 @@ const { loginSuccess, logoutSuccess } = slice.actions;
 /**
  * agregue aca la logica para incrustar los usuarios
  */
-export const login =
-  ({ username, password }) =>
-  async (dispatch) => {
-    try {
-      let data = await axios.post("http://192.168.1.10:3001/login", {
-        username,
-        password,
-      });
-      console.log("data: ", data);
-      dispatch(loginSuccess());
-    } catch (e) {
-      console.log("e: ", e);
-      return console.error(e.message);
-    }
-  };
+export const login = ({ username, password }) => async (dispatch) => {
+	try {
+		let data = await axios.post('http://localhost:3001/login', {
+			username,
+			password
+		});
+		console.log('data: ', data);
+		dispatch(loginSuccess());
+	} catch (e) {
+		console.log('e: ', e);
+		return console.error(e.message);
+	}
+};
 
 /**
  * agregue aca la logica para desloguear usuario
  */
 export const logout = () => async (dispatch) => {
-  try {
-    // *****
-    return dispatch(logoutSuccess());
-  } catch (e) {
-    return console.error(e.message);
-  }
+	try {
+		// *****
+		return dispatch(logoutSuccess());
+	} catch (e) {
+		return console.error(e.message);
+	}
 };
