92f43f967e8a614adb48ffebc97023b16feb3acf
Fernando-Leiva <99757333+Fernando-Leiva@users.noreply.github.com>
2022-04-20 19:21:41 -0600

    feat: create crud message. (#2)

--- dsu-si-backend/schemas/message.schema.js
+++ dsu-si-backend/schemas/message.schema.js
@@ -1,1 +1,10 @@
 //add here messages schema
+
+const mongoose = require("mongoose")
+
+const Message = mongoose.Schema({
+	username: String,
+	message: String,
+})
+
+module.exports = mongoose.model("Message", Message)
